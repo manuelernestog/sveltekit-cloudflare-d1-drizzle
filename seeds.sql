4db64f3bd57ea73ad6142d9de596c91110f0c560
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2024-09-13 22:20:48 -0400

    creating script to genera seeds

--- seeds.sql
+++ seeds.sql
@@ -1,2 +1,1 @@
 INSERT INTO "products" (productId, name, price) VALUES (1, 'Apple', 250), (2, 'Banana', 100), (3, 'Cherry', 375); 
-INSERT INTO "products" (productId, name, price) VALUES (1, 'Apple', 250), (2, 'Banana', 100), (3, 'Cherry', 375); 
